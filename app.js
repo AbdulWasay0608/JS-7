f92904e8f26a7384faa998be09d7cd751a833c3f
AbdulWasay0608 <abdulwasay0608@gmail.com>
2023-05-27 16:47:14 +0500

    Work Complete

---
+++ app.js
@@ -0,0 +1,29 @@
+var div2 = document.getElementById('div1')
+
+function makeCircle() {
+    div2.style.borderRadius = "265px"
+    div2.style.width = "265px"
+    div2.style.height = "265px"
+    div2.style.backgroundColor = "red"
+    div2.style.margin = "auto"
+}
+
+
+    function makeRectangle() {
+        div2.style.width = "265px"
+        div2.style.height = "133px"
+        div2.style.backgroundColor = "green"
+        div2.style.margin = "auto"
+        div2.style.borderRadius = "0px"
+        }
+
+
+        function makeSquare() {
+            div2.style.width = "265px"
+            div2.style.height = "265px"
+            div2.style.backgroundColor = "blue"
+            div2.style.margin = "auto"
+            div2.style.borderRadius = "0px"
+    }
+        
+    
